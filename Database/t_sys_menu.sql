f3d5ab135e02c3bb300ce5781e51fb192faac17f
Dym13934772520 <147064026+Dym13934772520@users.noreply.github.com>
2024-04-28 02:06:36 +0100

    Add files via upload

---
+++ Database/t_sys_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO `t_sys_menu` VALUES (1, NULL, NULL, NULL, NULL, '0', NULL, NULL, 0, 'user delete', '/user/delete', NULL, NULL, NULL);
+INSERT INTO `t_sys_menu` VALUES (2, NULL, NULL, NULL, NULL, '0', NULL, NULL, 1, 'user delete', '/user/delete', NULL, NULL, NULL);
+INSERT INTO `t_sys_menu` VALUES (3, NULL, NULL, NULL, NULL, '0', NULL, NULL, 0, 'user update', '/user/update', NULL, NULL, NULL);
+INSERT INTO `t_sys_menu` VALUES (4, NULL, NULL, NULL, NULL, '0', NULL, NULL, 0, 'user findByModal', '/user/findByModal', NULL, NULL, NULL);
---
+++ Database/t_sys_role.sql
@@ -0,0 +1,4 @@
+INSERT INTO `t_sys_role` VALUES (0, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Administrator', NULL, NULL);
+INSERT INTO `t_sys_role` VALUES (1, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Customer', NULL, NULL);
+INSERT INTO `t_sys_role` VALUES (2, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Merchant', NULL, NULL);
+INSERT INTO `t_sys_role` VALUES (3, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Delivery man', NULL, NULL);
