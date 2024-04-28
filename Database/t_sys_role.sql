5007216984837af20708f45ff1301334a028437b
Dym13934772520 <147064026+Dym13934772520@users.noreply.github.com>
2024-04-28 02:03:39 +0100

    Delete Database directory

--- Database/t_sys_role.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `t_sys_role` VALUES (0, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Administrator', NULL, NULL);
-INSERT INTO `t_sys_role` VALUES (1, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Customer', NULL, NULL);
-INSERT INTO `t_sys_role` VALUES (2, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Merchant', NULL, NULL);
-INSERT INTO `t_sys_role` VALUES (3, NULL, NULL, NULL, NULL, '0', NULL, NULL, 'Delivery man', NULL, NULL);
